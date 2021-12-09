dec64b5c411eecda2ff77f87677360dbb8d48c6d
aa <aa>
2021-12-10 04:45:16 +0900

    로그인 후 메인화면 이동

--- Hello/MainViewController.swift
+++ Hello/MainViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 
 
 class MainViewController : UIViewController{
@@ -24,6 +24,13 @@ class MainViewController : UIViewController{
         super.viewWillAppear(animated)
         
         navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        hiLabel.text = """
+       환영합니다
+       \(email)님
+       """
     }
     
     
