ade47427d0ea773d7c6c16e92d21d24996eeb031
aa <aa>
2021-12-13 14:21:22 +0900

    회원가입 UI 기능 구현

--- Hello/EmailViewController.swift
+++ Hello/EmailViewController.swift
@@ -15,49 +15,52 @@ class EmailViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var newButton: UIButton!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //navigation bar 보이기
         navigationController?.navigationBar.isHidden = false
         
-        loginButton.layer.cornerRadius = 10
+        [loginButton,newButton].forEach{
+            $0?.layer.cornerRadius = 10
+            $0?.layer.shadowOpacity = 0.4
+            $0?.layer.shadowOffset = CGSize(width: 0, height: 3)
+        }
         
+       
         loginButton.isEnabled = false
+        
+        
         
         emailText.delegate = self
         passwordText.delegate = self
         
         emailText.becomeFirstResponder()
+        
     }
     
     
+   
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         //firebase 이메일 비밀번호인증
         let email = emailText.text ?? ""
         let password = passwordText.text ?? ""
         
         
-        //신규 사용자 생성
-        Auth.auth().createUser(withEmail: email, password: password){
-            [weak self] authResult, error in
+        Auth.auth().signIn(withEmail: email, password: password){
+            [weak self]_, error in
             guard let self = self else {return}
             
-            if let error = error {
-                let code = (error as NSError).code
-                switch code {
-                case 17007: //이미 가입한 계정일때
-                    self.loginUser(withEmail: email, password: password)
-                default:
-                    self.errorMessage.text = error.localizedDescription
-                }
+            if let error = error{
+                self.errorMessage.text = error.localizedDescription
             }
-            
             else{
                 self.showMainViewController()
             }
-            
-            
         }
+        
+        
+        
         
     }
     
@@ -81,7 +84,6 @@ class EmailViewController: UIViewController {
             }
         }
     }
-    
     
     
     
--- Hello/LoginViewController.swift
+++ Hello/LoginViewController.swift
@@ -17,6 +17,9 @@ class LoginViewController: UIViewController{
     
     @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        //guard 뒤에 따라붙는 코드의 실행 결과가 true일 때 코드가 계속 실행됩니다.
+        //if 구문과는 다르게 guard 구문은 항상 else 구문이 뒤에 따라와야 합니다.
+       // 만약 guard 뒤 따라오는 Bool 값이 false라면 else의 블록 내부 코드를 실행하게 됩니다.
         let config = GIDConfiguration(clientID: clientID)
         GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
           if let error = error {
@@ -33,14 +36,14 @@ class LoginViewController: UIViewController{
                 self.showMainViewController()
             }
         }
-    }
+    }//뷰 클라이언트 id가 구글 로그인 메서드에 전달된뒤 반환
     
     private func showMainViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
         mainViewController.modalPresentationStyle = .fullScreen
         navigationController?.show(mainViewController, sender: nil)
-    }
+    }// 메인 뷰 컨트롤러 이동
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +53,9 @@ class LoginViewController: UIViewController{
             $0?.layer.borderWidth = 1
             $0?.layer.backgroundColor = UIColor.orange.cgColor
             $0?.layer.cornerRadius = 10
-            $0?.layer.borderColor = UIColor.white.cgColor
+            $0?.layer.borderColor = UIColor.orange.cgColor
+            $0?.layer.shadowOpacity = 0.4
+            $0?.layer.shadowOffset = CGSize(width: 0, height: 3)
             
             
      
